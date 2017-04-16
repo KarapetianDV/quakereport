c3a959408f54f4bde3b7974d317f3317ae7fe7b2
KarapetianDV <karapetiandav@gmail.com>
2017-04-16 15:20:58 +0300

    Add Earthquake class for abstraction

---
+++ app/src/main/java/com/example/android/quakereport/Earthquake.java
@@ -0,0 +1,20 @@
+package com.example.android.quakereport;
+
+public class Earthquake {
+
+    private String magnitude;
+    private String place;
+    private String time;
+
+    public String getMagnitude() {
+        return magnitude;
+    }
+
+    public String getPlace() {
+        return place;
+    }
+
+    public String getTime() {
+        return time;
+    }
+}
